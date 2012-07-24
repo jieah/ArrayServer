75c485358d695612e81913a98e5c33af26f0aca4
Hugo <hugoshi@blob.(none)>
2012-07-24 16:08:17 -0400

    refactored runscript

--- blaze/server/scripts/run.py
+++ blaze/server/scripts/run.py
@@ -7,6 +7,7 @@ import gevent_zeromq
 gevent_zeromq.monkey_patch()
 import zmq
 
+import blaze.server.tests
 import blaze.server.redisutils as redisutils
 import blaze.server.blazeconfig as blazeconfig
 import blaze.server.blazeconfig.orderedyaml as orderedyaml
@@ -14,33 +15,36 @@ import blaze.server.blazenode as blazenode
 import blaze.server.blazebroker as blazebroker
 
 import redis
+import collections
 import sys
 import yaml
 import os
 import logging
+import time
 import posixpath as blazepath
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
 #load a directory full of hdf5 files
-def init_dir(datadir):
+def build_config(datadir, disco=None):
     # datadir will have redis.db, redis.log, and a data directory,
     # as well as a blaze.config
     config_path = os.path.join(datadir, 'blaze.config')
-    if not os.path.exists(config_path):
-        base_config = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                                   'default_config.yaml')
-        with open(base_config) as f:
-            config = f.read()
-        config = config % {'datapath' : datadir}
-        with open(config_path, 'w+') as f:
-            f.write(config)
-
-import os.path
-    
+    base_config = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                               'default_config.yaml')
+    with open(base_config) as f:
+        config = f.read()
+    config = config % {'datapath' : datadir}
+    yamlconfig =yaml.load(config, Loader=orderedyaml.OrderedDictYAMLLoader)
+    if disco is not None:
+        yamlconfig['disco'] = collections.OrderedDict([('type', 'disco'),
+                                                       ('connection', disco)])
+    with open(config_path, 'w+') as f:
+        f.write(yaml.dump(yamlconfig))
             
+import os.path
 import argparse
-def main():
+def argparser():
     parser = argparse.ArgumentParser(description='Start blaze')
     parser.add_argument('datapath', nargs="?")
     parser.add_argument(
@@ -63,15 +67,27 @@ def main():
         help='namespace of node, nodes can see all nodes within this namespace',
         default='main'
     )
+    
+    parser.add_argument(
+        '-d', '--disco',
+        help='disco host:port',
+        default=None
+    )
+    
     parser.add_argument('-nr', '--no-redis', action='store_true')
     parser.add_argument('-rh', '--redis-host', default='localhost')
     parser.add_argument('-rp', '--redis-port', default=6379)
     parser.add_argument('-sc', '--skip-config', action='store_true')
-    parser.add_argument('-rc', '--rebuild-config', action='store_true')    
-    args = parser.parse_args()
-    print args
+    parser.add_argument('-rc', '--rebuild-config', action='store_true')
+    return parser
+
+def start_blaze(args):
     servername = args.server_name
-    datapath = os.path.abspath(args.datapath)
+    if args.datapath is None:
+        datapath = os.path.abspath(os.path.dirname(blaze.server.tests.__file__))
+        datapath = os.path.join(datapath, 'data')
+    else:
+        datapath = os.path.abspath(args.datapath)
     print 'datapath', datapath
     if not args.no_redis:
         assert args.redis_host == 'localhost', 'cannot start redis on another host'
@@ -80,18 +96,29 @@ def main():
             datapath,
             data_file=os.path.join(datapath, 'redis.db'),
             log_file=os.path.join(datapath, 'redis.log'))
-    init_dir(datapath)
-    data = yaml.load(open(os.path.join(datapath, 'blaze.config')).read(),
-                          Loader=orderedyaml.OrderedDictYAMLLoader)
+    time.sleep(0.1)
+    if not args.skip_config:
+        build_config(datapath, disco=args.disco)
+        data = yaml.load(open(os.path.join(datapath, 'blaze.config')).read(),
+                         Loader=orderedyaml.OrderedDictYAMLLoader)
+        config = blazeconfig.BlazeConfig(servername, host=args.redis_host,
+                                         port=args.redis_port, sourceconfig=data)
+    else:
+        config = blazeconfig.BlazeConfig(servername, host=args.redis_host,
+                                         port=args.redis_port)
     namespace = args.namespace
-    config = blazeconfig.BlazeConfig(servername, host=args.redis_host,
-                                     port=args.redis_port, sourceconfig=data)
     frontaddr = args.front_address
     backaddr = args.back_address
     node = blazenode.BlazeNode(backaddr, servername, config)
     node.start()
-    b = blazebroker.BlazeBroker(frontaddr, backaddr, config)
-    b.start()
+    broker = blazebroker.BlazeBroker(frontaddr, backaddr, config)
+    broker.start()
+    return proc, broker, node
+    
+def main():
+    parser = argparser()
+    args = parser.parse_args()
+    redisproc, broker, node = start_blaze(args)
     node.join()
     
 if __name__ == "__main__":
